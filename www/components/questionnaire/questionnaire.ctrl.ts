4756c4ad458d214f68ea9904c43ee03026e3b4c0
Drikus Roor <drikusroor@gmail.com>
2017-05-08 20:50:06 +0200

    Made initializing questions more error proof

--- www/components/questionnaire/questionnaire.ctrl.ts
+++ www/components/questionnaire/questionnaire.ctrl.ts
@@ -3,9 +3,20 @@ angular.module('ExperimentApp')
 
 .controller('QuestionnaireCtrl', function ($scope, $rootScope, $state, $stateParams, ConfigService, QUESTIONS) {
 
-  $scope.que = QUESTIONS[ConfigService.language];
-  console.log('Questionnaire is ' + ConfigService.language, $scope.que);
+  function setQuestions(language) {
+    try {
+      $scope.que = QUESTIONS[language];
+    } catch(e) {
+      console.log(e);
+      $scope.que = QUESTIONS.NL;
+    }
+    console.log('Questionnaire is ' + ConfigService.language, $scope.que);
+  }
 
+  // set $scope.que to the QUESTIONS in the right language
+  setQuestions(ConfigService.language);
+
+  // INIT empty form
   $scope.form = {};
 
   $scope.addQuestionnaireAnswer = function(question, answer){
